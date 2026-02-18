0012c2e1252d4de7fe5dd8e1c39bc327d795446f
rasila123 <rasilarecording@gmail.com>
2026-02-18 09:25:41 +0530

    supabase update 2

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://asgkhwwlcvgbldhuvzkv.supabase.co';
-const supabaseKey = 'sb_publishable_0CLHv-13aS1mbohVrGnNjA_szTF2wZJ';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzZ2tod3dsY3ZnYmxkaHV2emt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMzk3NzQsImV4cCI6MjA4NjkxNTc3NH0.DdF4j9gmi6hQYylbD5DtgXzLrTqnyM8pGFpIcWA3C6g';
 export const supabase = createClient(supabaseUrl, supabaseKey);
